9598683da547695f5b870f57ef9b32d5c9825659
Jesus Salcedo <jesalceg@gmail.com>
2025-03-22 10:53:41 -0500

    refactor: adjust the main options

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 from colorama import Fore, Style
 import functions
+import argparse
 
-ascii_art_colored = [
-    Fore.GREEN    + " (                                           ",
-    Fore.GREEN  + " )\ )                                         ",
-    Fore.GREEN + "(()/(     (       )                  (       ",
-    Fore.GREEN   + " /(_)) (  )(   ( /(  `  )   `  )    ))\  (   ",
-    Fore.GREEN+ "(_))   )\(()\  )(_)) /(/(   /(/(   /((_) )\  ",
-    Fore.GREEN   + "/ __| ((_)((_)((_)_ ((_)_\ ((_)_\ (_))( ((_) ",
-    Fore.CYAN  + "\__ \/ _|| '_|/ _` || '_ \)| '_ \)| || |(_-< ",
-    Fore.CYAN    + "|___/\__||_|  \__,_|| .__/ | .__/  \_,_|/__/ ",
-    Fore.CYAN + "                    |_|    |_|               ",
-    Style.RESET_ALL  # Restart
-]
+
+def main():
+    """
+    Show the main features arguments to choice when we need to scrapp something
+    """
+    parser = argparse.ArgumentParser(description="Universal Web Scrapper")
+    parser.add_argument('url', help='Websites URL to extract data.')
+    parser.add_argument('rules', help="JSON file with the extraction rules.")
+    parser.add_argument('--dynamic', action='store_true', help='If is a dynamic website, use selenium')
+    parser.add_argument('--output', default='output.json', help='Output file name')
+    parser.add_argument('--format', choices=['json', 'csv'], default='json', help='Output file extension')
+
+
+def show_banner():
+    """
+    Main identifier banner
+    """
+    banner = [
+        Fore.GREEN    + " (                                           ",
+        Fore.GREEN  + " )\ )                                         ",
+        Fore.GREEN + "(()/(     (       )                  (       ",
+        Fore.GREEN   + " /(_)) (  )(   ( /(  `  )   `  )    ))\  (   ",
+        Fore.GREEN+ "(_))   )\(()\  )(_)) /(/(   /(/(   /((_) )\  ",
+        Fore.GREEN   + "/ __| ((_)((_)((_)_ ((_)_\ ((_)_\ (_))( ((_) ",
+        Fore.CYAN  + "\__ \/ _|| '_|/ _` || '_ \)| '_ \)| || |(_-< ",
+        Fore.CYAN    + "|___/\__||_|  \__,_|| .__/ | .__/  \_,_|/__/ ",
+        Fore.CYAN + "                    |_|    |_|               ",
+        Style.RESET_ALL  
+    ]
+    
+    for line in banner:
+        print(line)
 
 
 functions = functions.Functions()
 
-for line in ascii_art_colored:
-    print(line)
     
 options = [
     {"id": 1, "text": '1. Scrapp host'},
